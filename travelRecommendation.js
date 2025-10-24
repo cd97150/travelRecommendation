c041f867e919d01bd161b49b932e1f2417dac256
cd97150 <chloedr971@gmail.com>
2025-10-24 13:25:29 -0400

    research filter

---
+++ travelRecommendation.js
@@ -0,0 +1,36 @@
+function fetchResults(event) {
+    event.preventDefault();
+
+    const dest = document.getElementById('destination').value.trim().toLowerCase();
+    const apiUrl = "./travelRecommendation_api.json";
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+
+            result.innerHTML = "";
+
+            data.countries.forEach(country => {
+                if (country.name.toLowerCase() === dest) {
+                    console.log(country);
+                    country.cities.forEach(city => {
+                        const result = document.getElementById('result');
+                        //afficher tout les resultats
+                        result.innerHTML += `<article><h3>${city.name}</h3><p>${city.description}</p><button class="btn-green">Visit<button></article>`
+                    });
+                }
+
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            const result = document.getElementById('result');
+            result.innerHTML = `<p>Échec de la récupération des datas. Veuillez réessayer.</p>`;
+        });
+}
+
+
+
+
+document.getElementById('btnSearch').addEventListener('click', fetchResults);
+
