7be7168d5394ece8909f4347a014f5939a96fcf3
cd97150 <chloedr971@gmail.com>
2025-10-27 11:37:44 -0400

    modif css + filter

--- travelRecommendation.js
+++ travelRecommendation.js
@@ -4,32 +4,62 @@ function fetchResults(event) {
     const dest = document.getElementById('destination').value.trim().toLowerCase();
     const apiUrl = "./travelRecommendation_api.json";
     const result = document.getElementById('result');
+    let filter;
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             result.innerHTML = "";
+            if (dest === "beaches" || dest === "temples") {
+                switch (dest) {
+                    case "beaches":
+                        filter = data.beaches;
+                        break;
+                    case "temples":
+                        filter = data.temples;
+                        break;
+                    default:
+                        console.log("erreur non connu");
+
+                }
+
+                filter.forEach(elt => {
+                    console.log(elt);
+                    result.innerHTML += `
+                            <article>
+                                <img src="${elt.imageUrl}" alt="${elt.name}">
+                                <h3>${elt.name}</h3>
+                                <p>${elt.description}</p>
+                                <button class="btn-green">Visit</button>
+                            </article>
+                        `;
+
+                });
+
+            }
             data.countries.forEach(country => {
-                country.cities.forEach(city => {
+                country.cities.forEach(elt => {
                     if (
                         country.name.toLowerCase().includes(dest) ||
-                        city.name.toLowerCase().includes(dest) || 
-                        city.description.toLowerCase().includes(dest) 
+                        elt.name.toLowerCase().includes(dest) ||
+                        elt.description.toLowerCase().includes(dest)
                     ) {
                         result.innerHTML += `
                             <article>
-                                <img src="${city.imageUrl}" alt="${city.name}">
-                                <h3>${city.name}</h3>
-                                <p>${city.description}</p>
+                                <img src="${elt.imageUrl}" alt="${elt.name}">
+                                <h3>${elt.name}</h3>
+                                <p>${elt.description}</p>
                                 <button class="btn-green">Visit</button>
                             </article>
                         `;
                     }
                 });
             });
-            // Si rien n'est trouvé
+
             if (result.innerHTML === "") {
                 result.innerHTML = `<div class="no-result"><p>No results found for "${dest}"</p><div>`;
             }
+
         })
         .catch(error => {
             console.error('Error fetching data:', error);
@@ -37,7 +67,6 @@ function fetchResults(event) {
             result.innerHTML = `<p>Échec de la récupération des datas. Veuillez réessayer.</p>`;
         });
 }
-
 
 
 
